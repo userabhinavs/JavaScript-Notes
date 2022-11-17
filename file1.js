53c3638e9fa1bf0d96dc732343f3283114549be8
userabhinavs <sabhinav965@gmail.com>
2022-11-18 00:25:17 +0530

    first commit

---
+++ file1.js
@@ -0,0 +1,19 @@
+//console.log can print somthing on console
+console.log("hello world");
+
+//intro to variables
+
+// varibles can store some information
+// we can use that information later
+// we can change that information later
+
+//declare a variable
+
+var firstName = "Abhinav Shukla";
+
+//use a varible
+console.log(firstName);
+
+//change value
+firstName = "Lalit";
+console.log(firstName);
